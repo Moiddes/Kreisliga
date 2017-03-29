87cf4021577183314b6da5f6e9bb351130b28f05
Moiddes <mattes.ruckdeschel@tuhh.degit config --global user.name Moiddesgit commit -a -m Initial>
2017-03-29 12:16:27 +0200

    Spieleranzahl

--- src/simulation/Spiel.java
+++ src/simulation/Spiel.java
@@ -2,6 +2,8 @@ package simulation;
 
 public class Spiel {
 
+	public int Spieler = 22;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hallo3");
