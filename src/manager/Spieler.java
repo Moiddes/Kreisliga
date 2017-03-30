46af609c9b612988e8a3aca00dcd6b0a13c1feae
Stefan <Stefan@Stefan-PC.home>
2017-03-30 13:18:11 +0200

    Spieler neu

--- src/manager/Spieler.java
+++ src/manager/Spieler.java
@@ -2,46 +2,171 @@ package manager;
 
 public class Spieler {
 
-	private int speed;
-	private int strength;
-	private int morale;
-
-
+	//neutrale Werte
+	private int geschwindigkeit;
+	private int zweikampf;
+	private int stellungsspiel;
+	private int ausdauer;
+	private int kopfball;
+	//mentale Werte
+	private int moral;
+	private int selbstbewusstsein;
+	private int disziplin;
+	private int aggresivitat;
+	//defensive Werte
+	private int antizipation;
+	private int torwart;
+	//offensive Werte
+	private int schuss;
+	private int freistoss;
+	private int pass;
+	private int dribbling;
+	private int flanken;
+	//Team Werte
+	private String team;
+	private String position;
 
 	public Spieler() {
-		this.setSpeed(50);
-		this.setStrength(50);
-		this.setMorale(50);
+
 	}
 
-	public Spieler( int speed, int strength, int morale) {
-		this.setSpeed(speed);
-		this.setStrength(strength);
-		this.setMorale(morale);
+	public int getZweikampf() {
+		return zweikampf;
 	}
 
-	public int getSpeed() {
-		return speed;
+	public void setZweikampf(int zweikampf) {
+		this.zweikampf = zweikampf;
 	}
 
-	public void setSpeed(int speed) {
-		this.speed = speed;
+	public int getStellungsspiel() {
+		return stellungsspiel;
 	}
 
-	public int getStrength() {
-		return strength;
+	public void setStellungsspiel(int stellungsspiel) {
+		this.stellungsspiel = stellungsspiel;
 	}
 
-	public void setStrength(int strength) {
-		this.strength = strength;
+	public int getAusdauer() {
+		return ausdauer;
 	}
 
-	public int getMorale() {
-		return morale;
+	public void setAusdauer(int ausdauer) {
+		this.ausdauer = ausdauer;
 	}
 
-	public void setMorale(int morale) {
-		this.morale = morale;
-	}	
+	public int getKopfball() {
+		return kopfball;
+	}
+
+	public void setKopfball(int kopfball) {
+		this.kopfball = kopfball;
+	}
+
+	public int getMoral() {
+		return moral;
+	}
+
+	public void setMoral(int moral) {
+		this.moral = moral;
+	}
+
+	public int getSelbstbewusstsein() {
+		return selbstbewusstsein;
+	}
+
+	public void setSelbstbewusstsein(int selbstbewusstsein) {
+		this.selbstbewusstsein = selbstbewusstsein;
+	}
+
+	public int getDisziplin() {
+		return disziplin;
+	}
+
+	public void setDisziplin(int disziplin) {
+		this.disziplin = disziplin;
+	}
+
+	public int getAggresivitat() {
+		return aggresivitat;
+	}
+
+	public void setAggresivitat(int aggresivitat) {
+		this.aggresivitat = aggresivitat;
+	}
+
+	public int getAntizipation() {
+		return antizipation;
+	}
+
+	public void setAntizipation(int antizipation) {
+		this.antizipation = antizipation;
+	}
+
+	public int getTorwart() {
+		return torwart;
+	}
+
+	public void setTorwart(int torwart) {
+		this.torwart = torwart;
+	}
+
+	public int getSchuss() {
+		return schuss;
+	}
+
+	public void setSchuss(int schuss) {
+		this.schuss = schuss;
+	}
+
+	public int getFreistoss() {
+		return freistoss;
+	}
+
+	public void setFreistoss(int freistoss) {
+		this.freistoss = freistoss;
+	}
+
+	public int getPass() {
+		return pass;
+	}
+
+	public void setPass(int pass) {
+		this.pass = pass;
+	}
+
+	public int getDribbling() {
+		return dribbling;
+	}
+
+	public void setDribbling(int dribbling) {
+		this.dribbling = dribbling;
+	}
+
+	public int getFlanken() {
+		return flanken;
+	}
+
+	public void setFlanken(int flanken) {
+		this.flanken = flanken;
+	}
+	
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		this.team = team;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+
+
 
 }
--- src/simulation/Event.java
+++ src/simulation/Event.java
@@ -7,7 +7,5 @@ public class Event {
 	public Event() {
 		
 		
-	}
-	
-	 
+	} 
 }
